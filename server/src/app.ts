55a172e65b1781cce3ad0499365c375421fd1fd7
Aman Kumar Gupta <akgbytes@gmail.com>
2025-10-01 18:40:28 +0530

    Created a simple express server

---
+++ server/src/app.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+const port = process.env.PORT ?? 8080;
+
+app.use("/hello", (req, res) => {
+  res.send("hello there");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
