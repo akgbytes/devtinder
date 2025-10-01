a8ed7c2a9f921b4e0025964391996b813a231c87
Aman Kumar Gupta <akgbytes@gmail.com>
2025-10-01 18:55:31 +0530

    Implement MongoDB connection

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,8 @@
 import "dotenv/config";
 import express from "express";
+import { connectDB } from "./config/db";
+
+connectDB();
 
 const app = express();
 const port = process.env.PORT ?? 8080;
---
+++ server/src/config/db.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI as string);
+    console.log("MongoDB connected successfully!");
+  } catch (error: any) {
+    console.log(
+      "Error while connecting to MongoDB: ",
+      error?.message || "Unknown error"
+    );
+    process.exit(1);
+  }
+};
