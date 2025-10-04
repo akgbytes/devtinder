1c3c60b859e374ff3afb00ac26ea6bcd58eebd1f
Aman Kumar Gupta <akgbytes@gmail.com>
2025-10-04 08:18:45 +0530

    Started frontend with Vite + React and setup HeroUI

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@heroui/react";
+
+const App = () => {
+  return (
+    <div>
+      <Button color="primary">Click Me</Button>
+      <h1 className="text-green-600">Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
